0f66978b8e8ce8c4eb4d94ceb5b24578efda609c
jackmackle20 <jackmackle@icloud.com>
2023-12-19 16:46:40 -0600

    setting up project model with actual projects

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from projects.models import Project
 
-# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+
+admin.site.register(Project, ProjectAdmin)
+
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from projects.models import Project
+from django.shortcuts import get_object_or_404
 
 def project_index(request):
     projects = Project.objects.all()
@@ -7,3 +8,15 @@ def project_index(request):
         "projects": projects
     }
     return render(request, "projects/project_index.html", context)
+
+def project_detail(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    template_name = f'projects/project_detail_{project_id}.html'
+    return render(request, template_name, {'project': project})
+
+"""
+def project_detail(request, project_id):
+    project = Project.objects.get(pk=project_id)
+    template_name = f'pages/project_detail_{project_id}.html'
+    return render(request, template_name, {'project': project})
+"""
