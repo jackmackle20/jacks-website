a11a76918181e7dc2aa419b890224ee9097619b3
jackmackle20 <jackmackle@icloud.com>
2024-01-14 14:50:16 -0600

    removing pywin

--- jacks_website/settings.py
+++ jacks_website/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n#&4e@mjo78sl*u9u^%)v_q)eu1)xk5n+qzh@_9jzsz37=g05b'
+#SECRET_KEY = 'django-insecure-n#&4e@mjo78sl*u9u^%)v_q)eu1)xk5n+qzh@_9jzsz37=g05b'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-n#&4e@mjo78sl*u9u^%)v_q)eu1)xk5n+qzh@_9jzsz37=g05b')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
